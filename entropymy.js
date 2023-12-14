f5f27f65692fe953a8ba7d86819f95081cd8ec1f
SashaSerd <145590030+SashaSerd@users.noreply.github.com>
2023-12-14 23:05:57 +0500

    Add files via upload

---
+++ entropymy.js
@@ -0,0 +1,25 @@
+str = 'abrakadabra';
+
+alph= new Array();
+for (let i = 0; i < str.length; i++){
+	if (alph[str[i]])
+		alph[str[i]]++;
+	else
+		alph[str[i]] = 1;
+}
+
+let power = 0;
+for (let i in alph){
+	power++;
+	alph[i] /= str.length;
+}
+let entropy = 0;
+if (power > 1){
+	for (let i in alph){
+		entropy -= alph[i]* Math.log(alph[i]);
+	}
+	entropy /= Math.log(power);
+}
+console.log(alph)
+console.log(entropy)
+
